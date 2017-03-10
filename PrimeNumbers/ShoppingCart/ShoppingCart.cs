ec7ed82b93587e683ffcd7ac08909bdb4e45d44e
James.Rodden <James.Rodden@DEV-JAMESR>
2017-03-10 10:58:09 +0000

    Second test

--- PrimeNumbers/ShoppingCart/ShoppingCart.cs
+++ PrimeNumbers/ShoppingCart/ShoppingCart.cs
@@ -1,10 +1,32 @@
-﻿namespace PrimeNumbers.ShoppingCart
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeNumbers.ShoppingCart
 {
     class ShoppingCart
     {
+        private readonly IEnumerable<ShoppingItem> _contents;
+
+        public ShoppingCart(IEnumerable<ShoppingItem> contents)
+        {
+            _contents = contents;
+        }
+
         public double CalculatePrice()
         {
-            return 0;
+            return _contents.Sum(shoppingItem => shoppingItem.Price);
+        }
+    }
+
+    class ShoppingItem
+    {
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public ShoppingItem(double price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
         }
     }
 }
--- PrimeNumbers/ShoppingCart/ShoppingCartTests.cs
+++ PrimeNumbers/ShoppingCart/ShoppingCartTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace PrimeNumbers.ShoppingCart
 {
@@ -7,8 +8,17 @@ namespace PrimeNumbers.ShoppingCart
         [Test]
         public void EmptyCartShouldCostNothing()
         {
-            var shoppingCart = new ShoppingCart();
+            
+            var shoppingCart = new ShoppingCart(new List<ShoppingItem>());
             Assert.That(shoppingCart.CalculatePrice(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SingleItemShouldCostValue()
+        {
+            var shoppingItem = new ShoppingItem(10,1);
+            var shoppingCart = new ShoppingCart(new List<ShoppingItem> { shoppingItem });
+            Assert.That(shoppingCart.CalculatePrice(), Is.EqualTo(10));
         }
     }
 }
