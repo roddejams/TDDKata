e94fed126d16ed9734c29b56817187ecf5e0c3ea
James.Rodden <James.Rodden@DEV-JAMESR>
2017-03-10 10:44:32 +0000

    Move into directory

--- PrimeNumbers/Primes/PrimeNumberGenerator.cs
+++ PrimeNumbers/Primes/PrimeNumberGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace PrimeNumbers
+namespace PrimeNumbers.Primes
 {
     public class PrimeNumberGenerator
     {
--- PrimeNumbers/Primes/PrimeNumberGeneratorTests.cs
+++ PrimeNumbers/Primes/PrimeNumberGeneratorTests.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
-namespace PrimeNumbers
+namespace PrimeNumbers.Primes
 {
     class PrimeNumberGeneratorTests
     {
