66ac42e536170a50848866a46895066d70dc96e1
James.Rodden <James.Rodden@DEV-JAMESR>
2017-03-10 10:14:32 +0000

    First failing test!

---
+++ PrimeNumbers/PrimeNumberGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    public class PrimeNumberGenerator
+    {
+        public IEnumerable<int> GetPrimes()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumberGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace PrimeNumbers
+{
+    class PrimeNumberGeneratorTests
+    {
+        [Test]
+        public void FirstPrimeNumberIsTwo()
+        {
+            var primeNumberGenerator = new PrimeNumberGenerator();
+            var listOfPrimes = primeNumberGenerator.GetPrimes().ToList();
+
+            Assert.That(listOfPrimes[0], Is.EqualTo(2));
+        }
+    }
+}
